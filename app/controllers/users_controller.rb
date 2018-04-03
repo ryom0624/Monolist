6999a9d7d833e1696ba31024c3c670ab698f5fac
ryo.murakami <ryo.m0624@gmail.com>
2018-04-03 07:22:48 +0000

    user registation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   @user = User.new(user_params)
+   if @user.save
+     flash[:success] = "ユーザの登録が完了しました"
+     redirect_to @user
+   else
+     flash.now[:denger] = "ユーザの登録に失敗しました"
+     render :new
+   end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+end
